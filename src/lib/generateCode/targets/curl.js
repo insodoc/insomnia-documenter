37be433efab8b9d1273e088d09369654fc5fa6f4
Arnaud Lier <zeprofdecoding@gmail.com>
2020-04-21 13:00:35 +0200

    Update curl.js

--- src/lib/generateCode/targets/curl.js
+++ src/lib/generateCode/targets/curl.js
@@ -2,30 +2,32 @@ import buildUrl from '../helpers/buildUrl';
 import escape from '../helpers/escape';
 
 export default function curl(url, req) {
-  let code = `curl --request ${req.method} \\\n`;
-  code += `  --url ${buildUrl(url, req)} \\\n`;
+  let code = `curl "${buildUrl(url, req)}" \\\n`;
+
   req.headers.forEach(header => {
-    code += `  --header '${header.name}: ${header.value}' \\\n`;
+    code += `  -H '${header.name}: ${header.value}' \\\n`;
   });
 
   if (req.authHeader) {
-    code += `  --header '${req.authHeader.name}: ${escape(req.authHeader.value)}' \\\n`;
+    code += `  -H '${req.authHeader.name}: ${escape(req.authHeader.value)}' \\\n`;
   }
+  
+  code += `  -X ${req.method} \\\n`;
 
   if (req.cookies && req.cookies.length) {
     req.cookies.forEach(cookie => {
-      code += `  --cookie '${cookie.key}'='${cookie.value}' \\\n`;
+      code += `  -b '${cookie.key}'='${cookie.value}' \\\n`;
     });
   }
 
   if (req.body && req.body.params && req.body.params.length) {
     req.body.params.forEach(param => {
-      code += `  --form '${param.name}=${param.value}' \\\n`;
+      code += `  -F '${param.name}=${param.value}' \\\n`;
     });
   }
 
   if (req.body && req.body.text) {
-    code += `  --data '${req.body.text.replace(/\t/g, '  ')}' \\\n`;
+    code += `  -d '${req.body.text.replace(/\t/g, '  ')}' \\\n`;
   }
 
   return code.slice(0, -2);
