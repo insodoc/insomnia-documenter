0d07b861c2425995937c2a0206a9dc1fbcc0b32d
Michael Baudino <michael.baudino@alpine-lab.com>
2020-06-10 16:33:15 +0200

    Fix basic HTTP authentication header generation
    It was previously missing the "Basic " prefix in the header value.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+### Fixed
+- Add mandatory `Basic ` prefix to `"Authorization"` header (thanks to @michaelbaudino)
+
 ## 0.3.2 - 2020-01-09
 ### Added
 - Tutorial and demo video in the README
--- src/lib/generateCode/auth/basic.test.js
+++ src/lib/generateCode/auth/basic.test.js
@@ -9,18 +9,22 @@ describe('Basic authentication', function () {
   describe('#getBasicAuthHeader', function () {
     it('should properly convert username:password string to base64', function () {
       const result = getBasicAuthHeader('joe', 'joe1234');
+      const [method, value] = result.value.split(' ');
       expect(result.name).to.eql('Authorization');
-      return expect(Buffer.from(result.value, 'base64').toString()).to.eql('joe:joe1234');
+      expect(method).to.eql('Basic');
+      return expect(Buffer.from(value, 'base64').toString()).to.eql('joe:joe1234');
     });
 
     it('should not encode username if not provided', function () {
       const result = getBasicAuthHeader(null, 'joe1234');
-      return expect(Buffer.from(result.value, 'base64').toString()).to.eql(':joe1234');
+      const [_method, value] = result.value.split(' ');
+      return expect(Buffer.from(value, 'base64').toString()).to.eql(':joe1234');
     });
 
     it('should not encode password if not provided', function () {
       const result = getBasicAuthHeader('joe');
-      return expect(Buffer.from(result.value, 'base64').toString()).to.eql('joe:');
+      const [_method, value] = result.value.split(' ');
+      return expect(Buffer.from(value, 'base64').toString()).to.eql('joe:');
     });
   });
 });
