3b08324bf8b9b7861f82e4a6b8be6834d3ce733a
Jeffrey Emakpor <jeffreyefemena4@gmail.com>
2021-06-18 00:45:08 +0000

    fix formatEnv falsy input paramater

--- src/lib/formatEnv.test.js
+++ src/lib/formatEnv.test.js
@@ -1,16 +1,20 @@
-import { expect } from 'chai';
-import formatEnv from './formatEnv';
+import { expect } from "chai";
+import formatEnv from "./formatEnv";
 
-describe('formatEnv', function () {
-  it('should replace env vars', function () {
-    return expect(formatEnv('The url is {{url}} and {{_.url}}')).to.eql(
+describe("formatEnv", function () {
+  it("should replace env vars", function () {
+    return expect(formatEnv("The url is {{url}} and {{_.url}}")).to.eql(
       'The url is <span class="env-variable">url</span> and <span class="env-variable">url</span>'
     );
   });
 
-  it('should replace env vars with spaces', function () {
-    return expect(formatEnv('The url is {{  url }} and {{ _.url  }}')).to.eql(
+  it("should replace env vars with spaces", function () {
+    return expect(formatEnv("The url is {{  url }} and {{ _.url  }}")).to.eql(
       'The url is <span class="env-variable">url</span> and <span class="env-variable">url</span>'
     );
+  });
+
+  it("should return an empty string for null input parameter", function () {
+    return expect(formatEnv(null)).to.eql("");
   });
 });
