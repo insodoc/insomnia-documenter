9ec971abdc41d27ee7c6ff34b21f2fd129c95b23
Hammed Oyedele <itz.harmid@gmail.com>
2021-08-23 10:41:24 +0100

    fix: the a11y issues in group component

--- src/components/content/Group.svelte
+++ src/components/content/Group.svelte
@@ -2,11 +2,12 @@
   import applyEnv from '../../lib/applyEnv';
 
   import showdown from 'showdown';
+
   const markdown = new showdown.Converter({
     simplifiedAutoLink: true,
     openLinksInNewWindow: true,
     excludeTrailingPunctuationFromURLs: true,
-    tables: true
+    tables: true,
   });
 
   export let group;
@@ -14,14 +15,15 @@
 
   $: groupData = {
     name: applyEnv(group.name, env),
-    description: applyEnv(group.description, env)
+    description: applyEnv(group.description, env),
   };
 
   // The Insomnia JSON file seems to have a description field here, but I couldn't find
   // any way to change the description of the groups in Insomnia itself.
   // The declaration will stay here anyway in case there will be better support for
   // this added later on in Insomnia.
-  $: description = groupData.description && markdown.makeHtml(groupData.description);
+  $: description =
+    groupData.description && markdown.makeHtml(groupData.description);
 </script>
 
 <div class="row">
@@ -34,5 +36,5 @@
 
     <hr />
   </div>
-  <div class="right"></div>
+  <div class="right" />
 </div>
