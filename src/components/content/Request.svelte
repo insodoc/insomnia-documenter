4dd0368157e54c45e5dcf29ea8f52be05c424bbc
Hammed Oyedele <itz.harmid@gmail.com>
2021-08-23 10:41:33 +0100

    fix: the a11y issues in request component

--- src/components/content/Request.svelte
+++ src/components/content/Request.svelte
@@ -1,23 +1,22 @@
 <script>
   import hljs from '../../lib/highlight';
 
-  import ClipboardJS from 'clipboard';
   import showdown from 'showdown';
-  import generateCode from '../../lib/generateCode';
-  import ContentGenerator from '../../lib/content';
+  import ClipboardJS from 'clipboard';
   import formatEnv from '../../lib/formatEnv';
+  import ContentGenerator from '../../lib/content';
+  import generateCode from '../../lib/generateCode';
 
   import Table from './Table.svelte';
 
   const markdown = new showdown.Converter({
+    tables: true,
     simplifiedAutoLink: true,
     openLinksInNewWindow: true,
     excludeTrailingPunctuationFromURLs: true,
-    tables: true
   });
 
   export let request;
-  export let env;
   export let language = null;
   export let cookiejars;
 
@@ -32,7 +31,7 @@
     url: formatEnv(request.url),
     name: request.name,
     description: request.description,
-    exampleResponses: request.exampleResponses
+    exampleResponses: request.exampleResponses,
   };
 
   $: exampleCode = generateCode(request, request.url, language, cookiejars);
@@ -44,22 +43,29 @@
   $: hljs.highlightBlock(code);
   $: exampleHTML = code.outerHTML;
 
-  $: clipboard = copyButton && new ClipboardJS(copyButton, {
-    target: function () {
-      return codeElement;
-    }
-  });
-  $: clipboard && clipboard.on('success', function () {
-    copyText = 'Copied!';
-    setTimeout(() => copyText = 'Copy to Clipboard', 5000);
-  });
-  $: clipboard && clipboard.on('error', function (err) {
-    console.error(err);
-    copyText = 'Failed to copy :(';
-    setTimeout(() => copyText = 'Copy to Clipboard', 5000);
-  });
+  $: clipboard =
+    copyButton &&
+    new ClipboardJS(copyButton, {
+      target: function () {
+        return codeElement;
+      },
+    });
 
-  $: description = reqData.description && markdown.makeHtml(reqData.description);
+  $: clipboard &&
+    clipboard.on('success', function () {
+      copyText = 'Copied!';
+      setTimeout(() => (copyText = 'Copy to Clipboard'), 5000);
+    });
+
+  $: clipboard &&
+    clipboard.on('error', function (err) {
+      console.error(err);
+      copyText = 'Failed to copy :(';
+      setTimeout(() => (copyText = 'Copy to Clipboard'), 5000);
+    });
+
+  $: description =
+    reqData.description && markdown.makeHtml(reqData.description);
 
   function getClassForStatusCode(code) {
     if (!code) {
@@ -84,156 +90,163 @@
 </script>
 
 <div class="row">
-    <div class="left">
-        <div class="anchor" id={request._id}>&nbsp;</div>
-        <h3 class="request-title"><strong class={request.method.toLowerCase()}>{request.method}</strong> {reqData.name}
-        </h3>
-        <pre class="url">{@html reqData.url}</pre>
+  <div class="left">
+    <div class="anchor" id={request._id}>&nbsp;</div>
+    <h3 class="request-title">
+      <strong class={request.method.toLowerCase()}>{request.method}</strong>
+      {reqData.name}
+    </h3>
+    <pre class="url">{@html reqData.url}</pre>
 
-        {#if description}
-            <div class="description">{@html description}</div>
-        {/if}
+    {#if description}
+      <div class="description">{@html description}</div>
+    {/if}
 
-        <div class="tables">
+    <div class="tables">
+      {#if request.parameters && request.parameters.length}
+        <Table data={content.params()} />
+      {/if}
 
-            {#if request.parameters && request.parameters.length}
-                <Table data={content.params()}/>
-            {/if}
+      {#if (request.headers && request.headers.length) || (request.authentication && request.authentication.type)}
+        <Table data={content.headers()} />
+      {/if}
 
-            {#if (request.headers && request.headers.length) || (request.authentication && request.authentication.type)}
-                <Table data={content.headers()}/>
-            {/if}
-
-            {#if request.body && (request.body.text || request.body.params)}
-                <Table data={content.body()}/>
-            {/if}
-
-        </div>
-
-        <hr/>
+      {#if request.body && (request.body.text || request.body.params)}
+        <Table data={content.body()} />
+      {/if}
     </div>
-    <div class="right">
-        <div class="code-example">
+
+    <hr />
+  </div>
+  <div class="right">
+    <div class="code-example">
+      <div class="header">
+        <div class="title">Example request:</div>
+        <div class="copy">
+          <span bind:this={copyButton}>{copyText}</span>
+        </div>
+      </div>
+      <pre bind:this={codeElement}>{@html exampleHTML}</pre>
+    </div>
+    {#if reqData.exampleResponses && reqData.exampleResponses.length}
+      {#each reqData.exampleResponses as example}
+        {#if example.value}
+          <div
+            class={'code-example example-response ' +
+              getClassForStatusCode(example.code)}
+          >
             <div class="header">
-                <div class="title">Example request:</div>
-                <div class="copy">
-                    <a href="javascript:" bind:this={copyButton}>{copyText}</a>
-                </div>
+              <div class="title">
+                Example response{example.code && example.code.length
+                  ? ` - ${example.code}`
+                  : ''}:
+              </div>
             </div>
-            <pre bind:this={codeElement}>{@html exampleHTML}</pre>
-        </div>
-        {#if reqData.exampleResponses && reqData.exampleResponses.length}
-            {#each reqData.exampleResponses as example}
-                {#if example.value}
-                    <div class={'code-example example-response ' + getClassForStatusCode(example.code)}>
-                        <div class="header">
-                            <div class="title">Example
-                                response{ example.code && example.code.length ? ` - ${example.code}` : '' }:
-                            </div>
-                        </div>
-                        <pre>{@html hljs.highlightAuto(example.value).value}</pre>
-                    </div>
-                {/if}
-            {/each}
+            <pre>{@html hljs.highlightAuto(example.value).value}</pre>
+          </div>
         {/if}
-    </div>
+      {/each}
+    {/if}
+  </div>
 </div>
 
 <style>
-	.anchor {
-		display: block;
-		position: relative;
-		top: -60px;
-		visibility: hidden;
-		height: 0;
-	}
+  .anchor {
+    display: block;
+    position: relative;
+    top: -60px;
+    visibility: hidden;
+    height: 0;
+  }
 
-	pre.url {
-		padding: 8px;
-		background: #e9e9e9;
-		border: 1px solid #d4d4d4;
-		border-radius: 2px;
-		overflow-x: auto;
-	}
+  pre.url {
+    padding: 8px;
+    background: #e9e9e9;
+    border: 1px solid #d4d4d4;
+    border-radius: 2px;
+    overflow-x: auto;
+  }
 
-	.code-example .header {
-		display: flex;
-		justify-content: space-between;
-		background: #404040;
-		color: #fff;
-		font-size: 14px;
-		font-weight: 600;
-	}
+  .code-example .header {
+    display: flex;
+    justify-content: space-between;
+    background: #404040;
+    color: #fff;
+    font-size: 14px;
+    font-weight: 600;
+  }
 
-	.code-example .header .title, .code-example .header .copy a {
-		padding: 8px 15px;
-	}
+  .code-example .header .title,
+  .code-example .header .copy span {
+    padding: 8px 15px;
+  }
 
-	.code-example .header .copy a {
-		display: inline-block;
-		text-decoration: none !important;
-		color: #fff;
-		background: #333;
-	}
+  .code-example .header .copy span {
+    cursor: pointer;
+    display: inline-block;
+    text-decoration: none !important;
+    color: #fff;
+    background: #333;
+  }
 
-	.code-example pre {
-		padding: 10px 15px;
-		border: 1px solid #404040;
-		border-top: 0;
-		margin: 0;
-		white-space: pre-wrap;
-		overflow-x: auto;
-	}
+  .code-example pre {
+    padding: 10px 15px;
+    border: 1px solid #404040;
+    border-top: 0;
+    margin: 0;
+    white-space: pre-wrap;
+    overflow-x: auto;
+  }
 
-	.example-response {
-		margin-top: 25px;
-	}
+  .example-response {
+    margin-top: 25px;
+  }
 
-	.example-response.default .header {
-		background: #675bc0;
-	}
+  .example-response.default .header {
+    background: #675bc0;
+  }
 
-	.example-response.default pre {
-		border-color: #675bc0;
-	}
+  .example-response.default pre {
+    border-color: #675bc0;
+  }
 
-	.example-response.info .header {
-		background: #3949ab;
-	}
+  .example-response.info .header {
+    background: #3949ab;
+  }
 
-	.example-response.info pre {
-		border-color: #3949ab;
-	}
+  .example-response.info pre {
+    border-color: #3949ab;
+  }
 
-	.example-response.success .header {
-		background: #43a047;
-	}
+  .example-response.success .header {
+    background: #43a047;
+  }
 
-	.example-response.success pre {
-		border-color: #43a047;
-	}
+  .example-response.success pre {
+    border-color: #43a047;
+  }
 
-	.example-response.redirect .header {
-		background: #6d4c41;
-	}
+  .example-response.redirect .header {
+    background: #6d4c41;
+  }
 
-	.example-response.redirect pre {
-		border-color: #6d4c41;
-	}
+  .example-response.redirect pre {
+    border-color: #6d4c41;
+  }
 
-	.example-response.client-error .header {
-		background: #fb8c00;
-	}
+  .example-response.client-error .header {
+    background: #fb8c00;
+  }
 
-	.example-response.client-error pre {
-		border-color: #fb8c00;
-	}
+  .example-response.client-error pre {
+    border-color: #fb8c00;
+  }
 
-	.example-response.server-error .header {
-		background: #e53935;
-	}
+  .example-response.server-error .header {
+    background: #e53935;
+  }
 
-	.example-response.server-error pre {
-		border-color: #e53935;
-	}
+  .example-response.server-error pre {
+    border-color: #e53935;
+  }
 </style>
